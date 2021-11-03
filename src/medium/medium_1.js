6cb33a1d3479095d3ecf6cdf05d0b4be57509f7c
solenn2000 <46977213+solenn2000@users.noreply.github.com>
2021-11-03 09:53:25 -0400

    try6

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -62,7 +62,7 @@ export function getStatistics(array) {
         min: Math.min(...array),
         max: Math.max(...array),
         variance: variance(array,getSum(array)/array.length),
-        standard_deviation: Math.sqrt(variance)
+        standard_deviation: Math.sqrt(variance(array,getSum(array)/array.length))
 
     }
 }
