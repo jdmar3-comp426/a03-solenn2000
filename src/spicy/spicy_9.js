f41672b38317abc8fe7f97b5e973ddfa34e45e91
solenn2000 <46977213+solenn2000@users.noreply.github.com>
2021-11-11 22:48:32 -0500

    updated

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -15,9 +15,9 @@
  *                from calling the function
  */
 export const repeat = (fn, n, ...params) => {
-    let arr =[]
+    let arr =[];
     for (let i = 0; i<n; i++){
-        let  a = fn(params);
+        let  a = fn(...params);
         arr.push(a);
     }
     return a;
