36cb153a0c26055769c0b0158faa18b58d4944aa
solenn2000 <46977213+solenn2000@users.noreply.github.com>
2021-11-14 11:10:57 -0500

    updated

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import {countArray} from "./src/mild/mild_1.js"
 import mpg_data from "./src/medium/data/mpg_data.js";
 import {allEvensAreOdd} from "./src/spicy/spicy_9.js"
-import {hasExactly} from "./src/spicy/spicy_9.js"
+import {moreStats} from "./src/medium/medium_2.js"
 
 
 
@@ -83,4 +83,4 @@ function everyEven(n) {
  }
  x => x % 2 === 0
 
-console.log(hasExactly([1, 0, 1, 0, 1], x => x === 0,3));
+console.log(moreStats);
